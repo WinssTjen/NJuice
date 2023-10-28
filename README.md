437896a721b072d117a73a3e2ce5f7c0a95dec62
WinssTjen <winsenind@gmail.com>
2023-10-28 10:54:13 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NJuice
+<<<<<<< HEAD
 Project Bussiness Application Development Group 9 NJuice
+=======
+>>>>>>> 865a129 (first commit)
---
+++ src/Main/Main.java
@@ -0,0 +1,20 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+		
+	}
+
+}
