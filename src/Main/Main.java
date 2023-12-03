9d3a05a9765a3015f5811ccb5b1d700ddf7b8a03
WinssTjen <winsenind@gmail.com>
2023-12-03 22:39:39 +0700

    Your Cart (Updated)

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -45,15 +45,15 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 	Window addWindow;
 	Juice j = new Juice(null, null, null, 0);
 	ListView<String> juiceList;
-	
+
 	// =========================
-	
+
 	Label juiceLabel, juicePrice, juiceDesc, juiceQty, juiceTotal;
 	Spinner<Integer> qtySpinner;
 	ComboBox<String> juiceTypeName;
 	Button addItemButton;
 	Background background;
-	
+
 
 
 	void initialize() {
@@ -65,7 +65,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 		hb = new HBox();
 		hb2 = new HBox();
 		sc = new Scene(bp, 1000, 750);
-		
+
 		// spacing
 		space = new Region();
 		HBox.setHgrow(space, Priority.ALWAYS);
@@ -96,84 +96,70 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 		yourCartDescLabel = new Label("Your cart is empty, try adding items!");
 		price = new Label("Total Price: 190000");
 		price.setFont(Font.font(null, FontWeight.SEMI_BOLD, 18));
-		
+
 		// toolbar
 		tb = new ToolBar();
-		
+
 		// list view
 		juiceList = new ListView<String>();
 		juiceList.setPrefHeight(300);
 		juiceList.setPrefWidth(600);
 		juiceList.setPadding(new Insets(20, 20, 20, 20));
-		
+
 		// window
 		addWindow = new Window("Add new item");
 		addWindow.setMaxHeight(400);
 		addWindow.setMaxWidth(800);
-		
-		// ======================================
-		// background
-//				background = new Background(new BackgroundFill(Color.WHITE, null, null));
-//				// scene & pane & window
-//				addWindow = new Window("Add new item");
-//				addWindow.setPrefHeight(500);
-//				addWindow.setPrefWidth(1000);
-//				addWindow.setBackground(background);
-//				bp2 = new BorderPane();
-//				sp = new StackPane();
-//				vb2 = new VBox();
-//				hb3= new HBox();
-//				sc = new Scene(sp, 600, 700);
-				
-				// region/spacing
-				space6 = new Region();
-				space7 = new Region();
-				
-				// label
-				juiceLabel = new Label("Juice: ");
-				juiceLabel.setFont(Font.font(null, FontWeight.NORMAL, 15));
-				juicePrice = new Label("Juice Price: ");
-				juicePrice.setFont(Font.font(null, FontWeight.NORMAL, 15));
-				juicePrice.setPadding(new Insets(10, 10, 10, 10));
-				juiceDesc = new Label("Description: ");
-				juiceDesc.setFont(Font.font(null, FontWeight.SEMI_BOLD, 15));
-				juiceQty = new Label("Quantity: ");
-				juiceQty.setFont(Font.font(null, FontWeight.NORMAL, 15));
-				juiceTotal = new Label("Total Price: ");
-				juiceTotal.setFont(Font.font(null, FontWeight.NORMAL, 15));
-				
-				// spinner
-				qtySpinner = new Spinner<Integer>();
-				qtySpinner.setPrefHeight(30);
-				qtySpinner.setPrefWidth(230);
-				
-				// combo box
-				juiceTypeName = new ComboBox<String>();
-				juiceTypeName.setPrefHeight(25);
-				juiceTypeName.setPrefWidth(240);
-				juiceTypeName.getItems().add("Avocado Avalanches");
-				juiceTypeName.getItems().add("Apple Adventure");
-				juiceTypeName.getItems().add("Berry Blast");
-				juiceTypeName.getItems().add("Mango Tango");
-				juiceTypeName.getItems().add("Citrus Crush");
-				juiceTypeName.getItems().add("Watermelon Wave");
-				juiceTypeName.getItems().add("Pear Pepper");
-				
-				// button
-				addItemButton = new Button("Add Item");
-				addItemButton.setPrefHeight(3);
-				addItemButton.setPrefWidth(98);
+
+		// region/spacing for window
+		space6 = new Region();
+		space7 = new Region();
+
+		// label window
+		juiceLabel = new Label("Juice: ");
+		juiceLabel.setFont(Font.font(null, FontWeight.NORMAL, 15));
+		juicePrice = new Label("Juice Price: ");
+		juicePrice.setFont(Font.font(null, FontWeight.NORMAL, 15));
+		juicePrice.setPadding(new Insets(10, 10, 10, 10));
+		juiceDesc = new Label("Description: ");
+		juiceDesc.setFont(Font.font(null, FontWeight.SEMI_BOLD, 15));
+		juiceQty = new Label("Quantity: ");
+		juiceQty.setFont(Font.font(null, FontWeight.NORMAL, 15));
+		juiceTotal = new Label("Total Price: ");
+		juiceTotal.setFont(Font.font(null, FontWeight.NORMAL, 15));
+
+		// spinner
+		qtySpinner = new Spinner<Integer>();
+		qtySpinner.setPrefHeight(30);
+		qtySpinner.setPrefWidth(230);
+
+		// combo box
+		juiceTypeName = new ComboBox<String>();
+		juiceTypeName.setPrefHeight(25);
+		juiceTypeName.setPrefWidth(240);
+		juiceTypeName.getItems().add("Avocado Avalanches");
+		juiceTypeName.getItems().add("Apple Adventure");
+		juiceTypeName.getItems().add("Berry Blast");
+		juiceTypeName.getItems().add("Mango Tango");
+		juiceTypeName.getItems().add("Citrus Crush");
+		juiceTypeName.getItems().add("Watermelon Wave");
+		juiceTypeName.getItems().add("Pear Pepper");
+
+		// button window
+		addItemButton = new Button("Add Item");
+		addItemButton.setPrefHeight(3);
+		addItemButton.setPrefWidth(98);
 	}
 
 	void layout() {
 		vb.getChildren().addAll(yourCartLabel);
 		vb.setAlignment(Pos.CENTER);
 		vb.setSpacing(10);
-		
+
 		juiceList.getItems().add("1x Avocado Avalanches - [Rp. 23500]");
 		juiceList.getItems().add("3x Berry Blast - [Rp. 73500]");
 		juiceList.getItems().add("2x Citrus Crush - [Rp. 43800]");
-		
+
 		if (juiceList.getItems().isEmpty()) {
 			vb.getChildren().addAll(yourCartDescLabel);
 			vb.setAlignment(Pos.CENTER);
@@ -181,53 +167,51 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 		}else {
 			hb2.getChildren().addAll(space4, juiceList, space5);
 			hb2.setAlignment(Pos.CENTER);
-			
+
 			vb.getChildren().add(hb2);
-			
+
 			vb.getChildren().add(price);
 			vb.setAlignment(Pos.CENTER);
 			vb.setSpacing(15);	
 		}
-		
+
 		hb.getChildren().addAll(addItem, deleteItem, checkout);
 		hb.setAlignment(Pos.CENTER);
 		hb.setSpacing(12);
 		vb.getChildren().add(hb);
-		
+
 		bp.setTop(tb);
 		bp.setCenter(vb);
 	}
-	
+
 	void windowLayout() {
-//		addWindow.getContentPane().getChildren().add(vb2);
-		
 		// nama juice
 		vb2.getChildren().add(juiceLabel);
 		vb2.setSpacing(8);
-		
+
 		// combo box + harga juice
 		hb3.getChildren().addAll(space6, juiceTypeName, juicePrice, space7);
 		hb3.setAlignment(Pos.CENTER);
 		vb2.getChildren().add(hb3);
 		vb2.setSpacing(8);
-		
+
 		// desc juice
 		vb2.getChildren().add(juiceDesc);
 		vb2.setSpacing(8);
-		
+
 		// quantity juice
 		vb2.getChildren().add(juiceQty);
 		vb2.setSpacing(8);
-		
+
 		// spiner
 		vb2.getChildren().add(qtySpinner);
 		SpinnerValueFactory<Integer> spinnerFact = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 1);
 		qtySpinner.setValueFactory(spinnerFact);
-		
+
 		vb2.getChildren().add(juiceTotal);
 		vb2.setSpacing(8);
 		vb2.getChildren().add(addItemButton);
-		
+
 		vb2.setAlignment(Pos.CENTER);
 	}
 
@@ -235,19 +219,19 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 		tb.getItems().addAll(space2, logoutBT, space, greetLabel, space3);
 		tb.setPrefHeight(22);
 	}
-	
+
 	void initAlert() {
 		deleteAlert = new Alert(AlertType.ERROR);
 		checkAlert = new Alert(AlertType.ERROR);
-		
+
 		deleteAlert.setHeaderText("Error");
 		deleteAlert.setContentText("Please choose which juice to delete");
 		deleteAlert.getDialogPane().getScene().getWindow();
-		
+
 		checkAlert.setHeaderText("Error");
 		checkAlert.setContentText("Your cart is empty");
 	}
-	
+
 	void setEventHandler() {
 		addItem.setOnAction(this);
 		deleteItem.setOnAction(this);
@@ -271,7 +255,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 		launch(args);
 
 	}
-	
+
 	public void openSecondaryWindow() {
 		background = new Background(new BackgroundFill(Color.WHITE, null, null));
 		addWindow = new Window("Add new item");
@@ -282,21 +266,21 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 		sp = new StackPane();
 		vb2 = new VBox();
 		hb3= new HBox();
-		
+
 		windowLayout();
-		
+
 		addWindow.getContentPane().getChildren().add(vb2);
-		
+
 		bp2.setCenter(addWindow);
 		bp2.setAlignment(vb2, Pos.TOP_CENTER);
-		
-		
+
+
 		Stage secondStage = new Stage();
 		secondStage.setScene(new Scene(bp2, 600, 700));
 		secondStage.showAndWait();
-		
+
 	}
-	
+
 	@Override
 	public void handle(ActionEvent event) {
 		if (event.getSource() == addItem) {
